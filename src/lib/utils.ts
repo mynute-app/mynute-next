92cfa04cd72ba7672c7827a8587e7e1ac848054e
ViitorAugusto <vitoraugusto2010201078@gmail.com>
2025-10-07 14:25:46 -0300

    refactor: remove unused data loader and related functions; clean up utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export function getBaseUrl() {
-  return process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3333";
-}
-
-export function apiBaseUrl() {
-  return process.env.BACKEND_URL;
+  return twMerge(clsx(inputs));
 }
